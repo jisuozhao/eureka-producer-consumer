149e39d32c1c7d76cc996651c56df11384a30fa3
jisuozhao <jisuozhao@vip.qq.com>
2019-05-09 21:08:09 +0800

    producer可以在minikube上正常运行

--- spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
+++ spring-cloud-producer/src/main/java/com/neo/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.neo.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+    @Autowired
+	private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/")
+    public String index() {
+        return "This is Producer!";
+    }
+
     @RequestMapping("/hello")
-    public String index(@RequestParam String name) {
+    public String hello(@RequestParam String name) {
         return "hello "+name+"，this is first messge";
     }
+
+    @RequestMapping("/services")
+	public List<String> services() {
+		return this.discoveryClient.getServices();
+	}
 }
