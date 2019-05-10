500cdfd57947e53813bde8ac68a55e66f8b8e8fe
jisuozhao <jisuozhao@vip.qq.com>
2019-05-10 21:46:33 +0800

    实现consumer调用producer

--- spring-cloud-consumer/src/main/java/com/neo/ConsumerApplication.java
+++ spring-cloud-consumer/src/main/java/com/neo/ConsumerApplication.java
@@ -3,13 +3,23 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 // import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+
+@RibbonClient(name = "spring-cloud-producer")
 public class ConsumerApplication {
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
--- spring-cloud-consumer/src/main/java/com/neo/controller/ConsumerController.java
+++ spring-cloud-consumer/src/main/java/com/neo/controller/ConsumerController.java
@@ -1,20 +1,36 @@
 package com.neo.controller;
-
-import com.neo.remote.HelloRemote;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
-    HelloRemote HelloRemote;
-	
-    @RequestMapping("/hello/{name}")
-    public String index(@PathVariable("name") String name) {
-        return HelloRemote.hello(name);
+	private DiscoveryClient discoveryClient;
+    
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/")
+    public String index() {
+        return "This is Consumer!";
     }
 
+    @RequestMapping("/call")
+    public String call() {
+        String result="调用spring-cloud-producer得到的返回结果:<br>";
+        result+=restTemplate.getForEntity("http://spring-cloud-producer/", String.class).getBody();
+        return result;
+    }
+
+    @RequestMapping("/services")
+	public List<String> services() {
+		return this.discoveryClient.getServices();
+	}
 }
